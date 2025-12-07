4e7d0cec7194d89cb3ba94c27e2381ba6acd1341
Serhii Chaichuk <128085928+chaichuk@users.noreply.github.com>
2025-12-07 21:11:16 +0100

    Add YASNO Dnipro URLs to constants

--- src/constants.js
+++ src/constants.js
@@ -17,3 +17,5 @@ export const {
 export const SHUTDOWNS_PAGE = "https://www.dtek-krem.com.ua/ua/shutdowns"
 export const LVIV_JSON_URL = "https://raw.githubusercontent.com/yaroslav2901/OE_OUTAGE_DATA/main/data/Lvivoblenerho.json"
 export const YASNO_KYIV_URL = "https://app.yasno.ua/api/blackout-service/public/shutdowns/regions/25/dsos/902/planned-outages"
+export const YASNO_DNIPRO_DNEM_URL = "https://app.yasno.ua/api/blackout-service/public/shutdowns/regions/3/dsos/301/planned-outages"
+export const YASNO_DNIPRO_CEK_URL = "https://app.yasno.ua/api/blackout-service/public/shutdowns/regions/3/dsos/303/planned-outages"
